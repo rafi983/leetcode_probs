864849a3745736813e1cad8f429abc552b87b0ce
rafi zaman <rafiirfan211@gmail.com>
2026-02-01 14:30:24 +0600

    93 problems solved

---
+++ minimum_add_to_make_parentheses_valid.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open_needed = 0;
+        int close_needed = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                close_needed++;
+            } else {
+                if (close_needed > 0) {
+                    close_needed--;
+                } else {
+                    open_needed++;
+                }
+            }
+        }
+
+        return open_needed + close_needed;
+    }
+};
---
+++ valid_anagram.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> count(26, 0);
+
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        for (char c : t) {
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
